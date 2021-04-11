ecded179edace73db56d37b399d31d822d537d0f
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-04-11 13:55:06 +0300

    Create index.java

---
+++ index.java
@@ -0,0 +1,14 @@
+import.os
+import.proccesor
+   @Ovveride
+     cilent("Cloud_Data")
+    impostor = java.cilent
+           install_impostor_version("35.01")
+    brawl_stars_cilent("ip")
+           mechafile.so
+             if("created_mechafile.so") = import.classes
+        server_data(import.brawlstars)
+     string; ipadresses(public.online_server)
+          mechafile.so("bytes, data,")
+      import.to("com.supercell.brawlstars")
+            
